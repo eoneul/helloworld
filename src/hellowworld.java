ee69a1c51b402200626a3b93f4c798ba24729488
조정아 <@DESKTOP-23N1BK7>
2017-11-23 19:08:47 +0900

    배고프당 추가

--- src/hellowworld.java
+++ src/hellowworld.java
@@ -2,6 +2,6 @@
 public class hellowworld {
     public static void main(String[] args ) {
     	System.out.println("정아현정현정지윤");
-    	System.out.println("모두 힘냅시다 허허");
+    	System.out.println("배고프당");
     }
 }
